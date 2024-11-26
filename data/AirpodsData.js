602b9d55a8797fecc840a177d70403f756ee8bab
jovination <jovination791@gmail.com>
2024-11-27 00:38:21 +0300

    first commit

---
+++ data/AirpodsData.js
@@ -0,0 +1,25 @@
+const AirpodsProductsData = [
+    {
+      id: 1,
+      name: "AirPods Pro 1",
+      features: [
+        "Immersive Noise Cancellation: Silence distractions for clear audio.",
+        "Comfortable Fit: Silicone tips in three sizes for all-day wear.",
+        "Adaptive EQ: Automatically tunes music for a rich listening experience.",
+      ],
+      price: "35,000/=",
+    },
+    {
+      id: 2,
+      name: "AirPods Pro 2",
+      features: [
+        "Pro-Level Noise Cancellation: Block out the world and focus on your sound.",
+        "USB-C Charging: Convenient and future-ready with a durable design.",
+        "Immersive Spatial Audio: Experience personalized, 3D sound tailored to your ears.",
+      ],
+      price: "55,000/=",
+    },
+  ];
+  
+  export default AirpodsProductsData;
+  
